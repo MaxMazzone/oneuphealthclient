d7ad85ee64c9f4a647ba62c3c414167ee1bd2b5f
stgeorge3 <amaxmazzone@gmail.com>
2020-06-28 13:57:46 -0400

    Updates

---
+++ precache-manifest.1840fe07e16ee0df07221ec208230928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502d495357ad0ace366a47b2967515d8",
+    "url": "/oneuphealthclient/index.html"
+  },
+  {
+    "revision": "673a07d6684bc1630df1",
+    "url": "/oneuphealthclient/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b87acda27318420ede62",
+    "url": "/oneuphealthclient/static/js/2.dc97b604.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/oneuphealthclient/static/js/2.dc97b604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673a07d6684bc1630df1",
+    "url": "/oneuphealthclient/static/js/main.60a077c2.chunk.js"
+  },
+  {
+    "revision": "719ea9b72ec8f8c50e55",
+    "url": "/oneuphealthclient/static/js/runtime-main.bc80580e.js"
+  }
+]);
